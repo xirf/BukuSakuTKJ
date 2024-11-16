441b3a23eda95dea904d7784071ae81162bf1844
xirf <55616872+xirf@users.noreply.github.com>
2024-11-16 18:02:47 +0700

    fix: ip addr

--- app/src/main/java/id/my/andka/bstkj/utils/getIPAddressInfo.kt
+++ app/src/main/java/id/my/andka/bstkj/utils/getIPAddressInfo.kt
@@ -28,12 +28,12 @@ fun getIPAddressInfo(ipAddressString: String): CalculationResult {
             ipClass = getIPClass(ipAddress),
             subnetMask = ipAddress.getNetworkMask().toCanonicalString().removeSlash(),
             subnetMaskBinary = ipAddress.getNetworkMask().toFormattedBinaryString(),
-            wildcardMask = ipAddress.networkMask.toString().removeSlash().split(".").reversed().joinToString("."),
+            wildcardMask = ipAddress.networkMask.toString().removeSlash().toWildcard(),
             firstHost = ipAddress.lower.toCanonicalString().removeSlash(),
-            lastHost = ipAddress.upper.toCanonicalString().removeSlash(),
+            lastHost = ipAddress.toIPv4().toBroadcastAddress().subtract(1),
             effectiveSubnets = 2.0.pow((32 - subnetMask).toDouble()).toInt(),
             effectiveHostsPerSubnet = (2.0.pow((32 - subnetMask).toDouble()) - 2).toInt(),
-            broadcastAddress = ipAddress.toMaxHost().toString().removeSlash(),
+            broadcastAddress = ipAddress.toIPv4().toBroadcastAddress().withoutPrefixLength().toString()
         )
 
         return CalculationResult.Success(ipInfo)
@@ -41,6 +41,16 @@ fun getIPAddressInfo(ipAddressString: String): CalculationResult {
         Log.e("IPCalculator", "Error calculating IP Address Info", e)
         return CalculationResult.Failure(e.message ?: "Unknown error")
     }
+}
+
+private fun IPAddress.subtract(count: Int): String {
+    return this.withoutPrefixLength().toString().split(".").mapIndexed { index, octet ->
+        if(index == 3) {
+            (octet.toInt() - count).toString()
+        }else {
+            octet
+        }
+    }.joinToString(".")
 }
 
 private fun IPAddress.toFormattedBinaryString(): String {
@@ -55,15 +65,23 @@ private fun String.removeSlash(): String {
     }
 }
 
+private  fun String.toWildcard(): String {
+    return this.split("."). map { octet -> 255 - octet.toInt() }.joinToString(".")
+}
+
 private fun getIPClass(ipAddress: IPAddress): String {
     if (ipAddress.isIPv4) {
         val firstOctet = ipAddress.toIPv4().segments[0].value.toInt()
-        return when (firstOctet) {
-            in 0..127 -> "Class A"
-            in 128..191 -> "Class B"
-            in 192..223 -> "Class C"
-            in 224..239 -> "Class D"
-            else -> "Class E"
+        return when {
+            firstOctet == 10 -> "Class A (Private)"
+            firstOctet == 172 && ipAddress.toIPv4().segments[1].value.toInt() in 16..31 -> "Class B (Private)"
+            firstOctet == 192 && ipAddress.toIPv4().segments[1].value.toInt() == 168 -> "Class C (Private)"
+            firstOctet in 0..127 -> "Class A (Public)"
+            firstOctet in 128..191 -> "Class B (Public)"
+            firstOctet in 192..223 -> "Class C (Public)"
+            firstOctet in 224..239 -> "Class D (Special)"
+            firstOctet in 240..255 -> "Class E (Special)"
+            else -> "N/A"
         }
     }
     return "N/A (IPv6)"
